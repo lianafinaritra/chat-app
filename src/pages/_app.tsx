9873e69c6a1650805373a7858478d1d27235e126
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-05-06 14:10:01 +0300

    feat: create chat page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import {Inter} from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
