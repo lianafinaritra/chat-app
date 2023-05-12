d6f427ba4019d67be9a9f19627aad6145cfe1871
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-05-12 16:07:25 +0300

    refactor: login form label

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import { useRouter } from 'next/router';
-import Authentication from "@/pages/Authentication";
 
 export default function Home() {
   const router = useRouter();
-  const [login, setLogin] = useState(false);
-  Authentication(router, login);
+  router.push('/login').catch((error) => {
+    console.log("Error during redirection in Login Form:", error);
+  });
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -38,15 +38,7 @@ export default function Login() {
                 <div className={loginStyle.container}>
                     <form className={loginStyle.form}>
                         <div className={loginStyle.formContainer}>
-                        <div className="relative z-0 w-2/3 mx-auto mb-6 group">
-                            <input type="text" id="floating_name"
-                                   className="block py-2.5 px-0 w-full text-sm text-gray-400 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-600 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
-                                   placeholder=" " required {...register("name")}/>
-                            <label htmlFor="floating_name"
-                                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom
-                                </label>
-                        </div>
-                            <div className="relative z-0 w-2/3 mx-auto mb-6 group">
+                            <div className="relative z-0 w-3/5 mx-auto mb-7 group">
                             <input type="email" id="floating_email"
                                    className="block py-2.5 px-0 w-full text-sm text-gray-400 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-600 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
                                    placeholder=" " required {...register("email")}/>
@@ -54,20 +46,13 @@ export default function Login() {
                                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
                                 </label>
                         </div>
-                            <div className="relative z-0 w-2/3 mx-auto mb-6 group">
+                            <div className="relative z-0 w-3/5 mx-auto mb-7 group">
                             <input type="password" id="floating_password"
                                    className="block py-2.5 px-0 w-full text-sm text-gray-400 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-600 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
                                    placeholder=" " required {...register("password")}/>
                             <label htmlFor="floating_password"
                                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mot de passe</label>
                         </div>
-                            <div className="relative z-0 w-2/3 mx-auto mb-6 group">
-                                <input type="password" id="floating_password"
-                                       className="block py-2.5 px-0 w-full text-sm text-gray-400 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-600 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
-                                       placeholder=" " required {...register("confirmPassword")}/>
-                                <label htmlFor="floating_password"
-                                       className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirmer</label>
-                            </div>
                             <div className={loginStyle.submit}>
                             <Button
                                 mt={1}
